4bfaa49578207b7b0437885b86492c2916e9f49d
matthijn <contact@matthijn.me>
2014-02-20 18:06:47 +0100

    Added loading scene and dots animation.

--- Strik.spritebuilder/STKLoadingScene.m
+++ Strik.spritebuilder/STKLoadingScene.m
@@ -8,6 +8,54 @@
 
 #import "STKLoadingScene.h"
 
+#define DOTS_DELAY 0.5f
+
+@interface STKLoadingScene()
+
+@property CCLabelTTF *connectingDots;
+
+@property BOOL connecting;
+
+@end
+
 @implementation STKLoadingScene
+
+- (void)sceneWillBegin
+{
+	self.connecting = YES;
+	
+	// Start the connection animation
+	[self performSelector:@selector(animateDots) withObject:nil afterDelay:0];
+}
+
+// Just changes between 1, 2 or 3 dots while connecting.
+- (void)animateDots
+{
+	NSString *dots = self.connectingDots.string;
+	
+	if([dots isEqualToString:@"."])
+	{
+		dots = @"..";
+	}
+	else if([dots isEqualToString:@".."])
+	{
+		dots = @"...";
+	}
+	else if([dots isEqualToString:@"..."])
+	{
+		dots = @"";
+	}
+	else
+	{
+		dots = @".";
+	}
+	
+	self.connectingDots.string = dots;
+	
+	if(self.connecting)
+	{
+		[self performSelector:@selector(animateDots) withObject:nil afterDelay:DOTS_DELAY];
+	}
+}
 
 @end

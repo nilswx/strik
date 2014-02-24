c85a4843499a5c4d1de45af214e10c201c174ae5
matthijn <contact@matthijn.me>
2014-02-24 11:32:32 +0100

    Forgot to submit timelinitem node.

---
+++ Strik.spritebuilder/STKTimelineItemNode.m
@@ -0,0 +1,13 @@
+//
+//  STKTimelineItemNode.m
+//  Strik
+//
+//  Created by Matthijn Dijkstra on 24/02/14.
+//  Copyright (c) 2014 Strik. All rights reserved.
+//
+
+#import "STKTimelineItemNode.h"
+
+@implementation STKTimelineItemNode
+
+@end

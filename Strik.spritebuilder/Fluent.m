2c27e81a0c2c2e310315e1de3ba10277cbb246f3
matthijn <contact@matthijn.me>
2014-03-05 14:27:58 +0100

    A period at the end of a sentence never killed anybody.

--- Strik.spritebuilder/Fluent.m
+++ Strik.spritebuilder/Fluent.m
@@ -14,7 +14,7 @@
 + (NSString *)fluentStringWithActor:(NSString *)actor action:(NSString *)action subject:(NSString *)subject atTime:(NSDate *)time
 {
 	// Todo: change behaviour based on localization
-	return [NSString stringWithFormat:@"%@ %@ %@ %@", actor, action, subject, [time dateTimeUntilNow]];
+	return [NSString stringWithFormat:@"%@ %@ %@ %@.", actor, action, subject, [time dateTimeUntilNow]];
 }
 
 @end

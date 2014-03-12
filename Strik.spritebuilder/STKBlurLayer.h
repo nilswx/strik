cbe061d756c88f06bd748ad51061287cb9728a17
Nils Weldon <nilsweldon@gmail.com>
2014-03-12 18:02:05 +0100

    Physics demo

---
+++ Strik.spritebuilder/STKBlurLayer.h
@@ -0,0 +1,13 @@
+//
+//  STKBlurLayer.h
+//  Strik
+//
+//  Created by Nils Wiersema on Mar 12, 2014.
+//  Copyright (c) 2014 Strik. All rights reserved.
+//
+
+#import "CCNodeColor.h"
+
+@interface STKBlurLayer : CCNodeColor
+
+@end
---
+++ Strik.spritebuilder/STKBlurLayer.m
@@ -0,0 +1,13 @@
+//
+//  STKBlurLayer.m
+//  Strik
+//
+//  Created by Nils Wiersema on Mar 12, 2014.
+//  Copyright (c) 2014 Strik. All rights reserved.
+//
+
+#import "STKBlurLayer.h"
+
+@implementation STKBlurLayer
+
+@end

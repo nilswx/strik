6756c0b032028b3af4e7ae50ca04d6338b1081d8
Nils Weldon <nilsweldon@gmail.com>
2014-03-12 18:06:48 +0100

    Added remaining experiment stuff of STKBlurLayer (wip)

--- Strik.spritebuilder/STKBlurLayer.m
+++ Strik.spritebuilder/STKBlurLayer.m
@@ -8,6 +8,24 @@
 
 #import "STKBlurLayer.h"
 
+#import "CCNode_Private.h"
+
 @implementation STKBlurLayer
+
+- (id)init
+{
+	if(self = [super initWithColor:PLAYER_ONE_COLOR])
+	{
+		self.shaderProgram = [CCGLProgram programWithVertexShaderFilename:@"example_Twist.vsh" fragmentShaderFilename:@"example_Blur.fsh"];
+		[self.shaderProgram link];
+	}
+	
+	return self;
+}
+
+- (void)update:(CCTime)delta
+{
+	[self.shaderProgram updateUniforms];
+}
 
 @end

8202ad3eef4e1e84526d1add54c52733c5900f6b
Nils Weldon <nilsweldon@gmail.com>
2014-03-19 11:37:49 +0100

    Added GameGlobals.h

--- Strik.spritebuilder/GameGlobals.h
+++ Strik.spritebuilder/GameGlobals.h
@@ -1,14 +1,24 @@
 //
-//  STKGlobals.h
+//  GameGlobals.h
 //  Strik
 //
 //  Created by Nils Wiersema on Mar 19, 2014.
 //  Copyright (c) 2014 Strik. All rights reserved.
 //
 
-#ifndef Strik_STKGlobals_h
-#define Strik_STKGlobals_h
+// Network host
+#define SERVER_HOST @"localhost" // TODO: change to load balancer
+#define SERVER_PORT 13381
 
+// Strik 'red'
+#define PLAYER_ONE_COLOR [CCColor colorWithRed:34.0f/255.0f green:189.0f/255.0f blue:175.0f/255.0f]
+#define PLAYER_ONE_LIGHT_COLOR [CCColor colorWithRed:145.0f/255.0f green:222.0f/255.0f blue:215.0f/255.0f]
 
+// Strik 'green'
+#define PLAYER_TWO_COLOR [CCColor colorWithRed:241.0f/255.0f green:75.0f/255.0f blue:106.0f/255.0f]
+#define PLAYER_TWO_LIGHT_COLOR [CCColor colorWithRed:229.0f/255.0f green:168.0f/255.0f blue:179.0f/255.0f]
 
-#endif
+// Status colors
+#define PLAYER_ONLINE_COLOR [CCColor colorWithRed:34.0f/255.0f green:189.0f/255.0f blue:60.0f/255.0f]
+#define PLAYER_BUSY_COLOR [CCColor colorWithRed:241.0f/255.0f green:192.0f/255.0f blue:75.0f/255.0f]
+#define PLAYER_OFFLINE_COLOR [CCColor colorWithRed:241.0f/255.0f green:75.0f/255.0f blue:75.0f/255.0f]

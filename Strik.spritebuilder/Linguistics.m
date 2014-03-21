42e7fa7ae3439590c06f598919c37773a57038c8
Nils Weldon <nilsweldon@gmail.com>
2014-03-21 16:12:38 +0100

    Neater timeline messages

--- Strik.spritebuilder/Linguistics.m
+++ Strik.spritebuilder/Linguistics.m
@@ -16,7 +16,7 @@
 + (NSString *)fluentStringWithActor:(NSString *)actor action:(NSString *)action subject:(NSString *)subject atTime:(NSDate *)time
 {
 	// Todo: change behaviour based on localization
-	return [NSString stringWithFormat:@"%@ %@ %@ %@.", actor, action, subject, [time dateTimeUntilNow]];
+	return [NSString stringWithFormat:@"%@ %@ %@ %@!", actor, action, subject, [time dateTimeUntilNow]];
 }
 
 + (NSString *)localizedNameFromCountryCode:(NSString *)countryCode

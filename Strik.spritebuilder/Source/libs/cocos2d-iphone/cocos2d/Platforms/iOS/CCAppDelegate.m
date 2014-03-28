53991497371a628b3bcfd7d0dd66cb4ac5f8f148
Nils Weldon <nilsweldon@gmail.com>
2014-03-28 22:17:44 +0100

    cocos2d debug stats can now be toggled through the configCocos2d.plist

--- Strik.spritebuilder/Source/libs/cocos2d-iphone/cocos2d/Platforms/iOS/CCAppDelegate.m
+++ Strik.spritebuilder/Source/libs/cocos2d-iphone/cocos2d/Platforms/iOS/CCAppDelegate.m
@@ -193,10 +193,10 @@ FindPOTScale(CGFloat size, CGFloat fixedSize)
 	
 	director.wantsFullScreenLayout = YES;
 	
-//#if DEBUG
+#if DEBUG
 	// Display FSP and SPF
 	[director setDisplayStats:[config[CCSetupShowDebugStats] boolValue]];
-//#endif
+#endif
 	
 	// set FPS at 60
 	NSTimeInterval animationInterval = [(config[CCSetupAnimationInterval] ?: @(1.0/60.0)) doubleValue];
